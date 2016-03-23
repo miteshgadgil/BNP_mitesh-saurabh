01c8a5e9b6f6fb7fbf4c52fbe83555434c8d08b1
miteshgadgil <mgadgil@ucsd.edu>
2016-03-22 20:05:06 -0700

    Main R-script

---
+++ Math289_BNP.R
@@ -0,0 +1,7 @@
+setwd("C:/Users/mitesh/Desktop/kaggle")
+d1 <- read.csv("sample_submission.csv")
+head(d1)
+data.test <- read.csv("test.csv")
+head(data.test)
+data.train <- read.csv("train.csv")
+head(data.train)
